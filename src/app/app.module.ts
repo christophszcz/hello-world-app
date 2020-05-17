d51e59d53ca07e2ae0c930f582da460a4c47720b
Chris Szczechowicz <info@cjszczechowicz.com>
2020-05-17 14:05:43 -0400

    Add task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { TableComponent } from './dashboard/table/table.component';
+import { TaskComponent } from './dashboard/task/task.component';
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    TableComponent
+    TableComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
